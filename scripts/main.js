4348ecab03d7ae019828d2afcc9a9797997cdb8e
Lauren Ashley <la.zaubi@gmail.com>
2022-09-10 21:15:30 -0700

    empty conatiner script file

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  const drawBarChart = function (data, options, element) {
+    let result = 'I am empty';
+
+    return result;
+  }
+
+  let data = [],
+      options = {},
+      element = '';
+
+  drawBarChart(data, options, element);
+
+});
